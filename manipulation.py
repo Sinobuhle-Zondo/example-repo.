4fe6768dfe52b3862aac4997f6ad6ff07eff6391
Sinobuhle Zondo <snobuhlezondo1@gmail.com>
2025-12-18 23:38:48 +0200

    Refactor manipulation.py
    Manipulating a string phrase from the user in order to print out the number of characters in a string, replace characters and reverse characters.

--- manipulation.py
+++ manipulation.py
@@ -1,18 +1,9 @@
 #get the special phrase from the user and perform string manipulations
 str_manip = input("please enter a qoute you love:\n")
-
 str_length = len(str_manip)
-
 print(str_length)
-
 last_character = str_manip[-1]
-
 print(last_character)
-
 print(str_manip.replace(last_character, "@"))
-
 print(str_manip[-3:][::-1])
-
 print(str_manip[0:2] + str_manip[-3:])
-
-
